7d1a2a874b5eff5c9f2232ce52a6e29b5cecefc4
sylvey <a.few.star@gmail.com>
2025-05-04 19:45:35 -0500

    fix bug

--- backend/databases/restaurants.py
+++ backend/databases/restaurants.py
@@ -5,22 +5,32 @@ def get_restaurants():
     data = request.get_json()
 
     cuisine_type = data.get("CuisineType")
-    price_min = data.get("PriceMin")
-    price_max = data.get("PriceMax")
+    price_min = data.get("PriceMin", 0.0)
+    price_max = data.get("PriceMax", 1e9)
     mood = data.get("Mood")
 
     conn, cursor = connect()
 
+    # 基礎查詢
     query = """
         SELECT DISTINCT r.RstrntID AS RestaurantID, r.RstrntName AS RestaurantName
         FROM Restaurant r
         JOIN Menu m ON r.RstrntID = m.RstrntID
         JOIN `Option` o ON m.OptID = o.OptID
-        WHERE o.CuisineType = %s
-          AND o.Mood = %s
-          AND m.Price BETWEEN %s AND %s
+        WHERE m.Price BETWEEN %s AND %s
     """
-    cursor.execute(query, (cuisine_type, mood, price_min, price_max))
+    params = [price_min, price_max]
+
+    # 加入條件
+    if cuisine_type:
+        query += " AND o.CuisineType = %s"
+        params.append(cuisine_type)
+
+    if mood is not None:
+        query += " AND o.Mood = %s"
+        params.append(mood)
+
+    cursor.execute(query, params)
 
     columns = [desc[0] for desc in cursor.description]
     results = [dict(zip(columns, row)) for row in cursor.fetchall()]
